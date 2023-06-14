9b410c6cd1f0ed3dd90edfe210d8c831fc51f5dc
abhishek <webmaster@bestway2india.com>
2023-06-14 17:53:28 +0530

    shopkeeper

--- routes/web.php
+++ routes/web.php
@@ -29,46 +29,48 @@ Route::get('admin/get-city', [EmployeeController::class, 'GetCity']);
 Route::get('employee/crnt_location', [ShopKeeperController::class, 'GetLocation']);
 
 Route::group(['middleware' => ['role:admin']], function () {
+    Route::prefix('admin')->group(function () {
 
-    // ********************** Admin Employee ********************************************
-    Route::get('admin/add-employee', [EmployeeController::class, 'Employee']);
-    Route::post('admin/employee-add', [EmployeeController::class, 'NewEmployee']);
-    Route::get('admin/active-employees', [EmployeeController::class, 'ActiveEmployees']);
-    Route::get('admin/inactive-employee', [EmployeeController::class, 'InactiveEmployees']);
-    Route::get('admin/employee-profile/{employee_id}', [EmployeeController::class, 'EmployeeProfile']);
-    Route::get('/admin/employee_status', [EmployeeController::class, 'EmployeeStatus']);
+        // ********************** Admin Employee ********************************************
+        Route::get('/add-employee', [EmployeeController::class, 'Employee']);
+        Route::post('/employee-add', [EmployeeController::class, 'NewEmployee']);
+        Route::get('/active-employees', [EmployeeController::class, 'ActiveEmployees']);
+        Route::get('/inactive-employee', [EmployeeController::class, 'InactiveEmployees']);
+        Route::get('/employee-profile/{employee_id}', [EmployeeController::class, 'EmployeeProfile']);
+        Route::get('/employee_status', [EmployeeController::class, 'EmployeeStatus']);
 
-    // ***************************** Admin Customer ***************************
+        // ***************************** Admin Customer ***************************
 
-    Route::view('admin/add-customer', 'admin.customer.add-customer');
-    Route::post('admin/new-customer', [CustomerController::class, 'NewCustomer']);
-    Route::get('admin/inactive-customers', [CustomerController::class, 'InActiveCustomers']);
-    Route::get('admin/active-customers', [CustomerController::class, 'ActiveCustomers']);
-    Route::get('/admin/customer_status', [CustomerController::class, 'CustomerStatus']);
-    Route::get('admin/customer-profile/{customer_id}', [CustomerController::class, 'CustomerProfile']);
-    Route::get('admin/update-customer/{customer_id}', [CustomerController::class, 'EditCustomer']);
-    Route::post('admin/edit-customer/{customer_id}', [CustomerController::class, 'UpdateCustomer']);
+        Route::view('/add-customer', 'admin.customer.add-customer');
+        Route::post('/new-customer', [CustomerController::class, 'NewCustomer']);
+        Route::get('/inactive-customers', [CustomerController::class, 'InActiveCustomers']);
+        Route::get('/active-customers', [CustomerController::class, 'ActiveCustomers']);
+        Route::get('/customer_status', [CustomerController::class, 'CustomerStatus']);
+        Route::get('/customer-profile/{customer_id}', [CustomerController::class, 'CustomerProfile']);
+        Route::get('/update-customer/{customer_id}', [CustomerController::class, 'EditCustomer']);
+        Route::post('/edit-customer/{customer_id}', [CustomerController::class, 'UpdateCustomer']);
 
-    // **************************** Admin Shop *********************************
+        // **************************** Admin Shop *********************************
 
-    Route::get('admin/local-shop-form', [ShopKeeperController::class, 'ShopForm']);
-    Route::post('admin/create-shop', [ShopKeeperController::class, 'StoreLocalShop']);
-    Route::get('admin/global-shop-form', [GlobalShopController::class, 'GlobalShop']);
-    Route::post('admin/create-global-shop', [GlobalShopController::class, 'GlobalShopStore']);
-    Route::get('admin/inactive-local-shops', [ShopKeeperController::class, 'InactiveLocalShop']);
-    Route::get('admin/active-local-shops', [ShopKeeperController::class, 'ActiveLocalShop']);
-    Route::get('admin/shop_status', [ShopKeeperController::class, 'ShopStatus']);
-    Route::get('admin/shop-profile/{shop_id}', [ShopKeeperController::class, 'ShopProfile']);
-    Route::get('admin/local-shop/{shop_id}', [ShopKeeperController::class, 'ShopUpdate']);
-    Route::get('admin/shop/shop_menu/delete/{id}', [ShopKeeperController::class, 'ShopMenuDelete']);
-    Route::get('admin/shop/shop_pic/delete/{id}', [ShopKeeperController::class, 'ShopPicDelete']);
-    Route::get('admin/shop/shop_aadhar_card/delete/{id}', [ShopKeeperController::class, 'ShopAdharDelete']);
-    Route::get('admin/shop/shop_pancard/delete/{id}', [ShopKeeperController::class, 'ShopPanDelete']);
-    Route::get('admin/shop/shop_driving/delete/{id}', [ShopKeeperController::class, 'ShopDrivingDelete']);
-    Route::get('admin/shop/shop_passport/delete/{id}', [ShopKeeperController::class, 'ShopPassportDelete']);
-    Route::get('admin/shop/shop_cv/delete/{id}', [ShopKeeperController::class, 'ShopCvDelete']);
-    Route::get('admin/shop/shop_agreement/delete/{id}', [ShopKeeperController::class, 'ShopAgreementDelete']);
-    Route::post('admin/update-shop/{shop_id}', [ShopKeeperController::class, 'UpdateShop']);
+        Route::get('/local-shop-form', [ShopKeeperController::class, 'ShopForm']);
+        Route::post('/create-shop', [ShopKeeperController::class, 'StoreLocalShop']);
+        Route::get('/global-shop-form', [GlobalShopController::class, 'GlobalShop']);
+        Route::post('/create-global-shop', [GlobalShopController::class, 'GlobalShopStore']);
+        Route::get('/inactive-local-shops', [ShopKeeperController::class, 'InactiveLocalShop']);
+        Route::get('/active-local-shops', [ShopKeeperController::class, 'ActiveLocalShop']);
+        Route::get('/shop_status', [ShopKeeperController::class, 'ShopStatus']);
+        Route::get('/shop-profile/{shop_id}', [ShopKeeperController::class, 'ShopProfile']);
+        Route::get('/local-shop/{shop_id}', [ShopKeeperController::class, 'ShopUpdate']);
+        Route::get('/shop/shop_menu/delete/{id}', [ShopKeeperController::class, 'ShopMenuDelete']);
+        Route::get('/shop/shop_pic/delete/{id}', [ShopKeeperController::class, 'ShopPicDelete']);
+        Route::get('/shop/shop_aadhar_card/delete/{id}', [ShopKeeperController::class, 'ShopAdharDelete']);
+        Route::get('/shop/shop_pancard/delete/{id}', [ShopKeeperController::class, 'ShopPanDelete']);
+        Route::get('/shop/shop_driving/delete/{id}', [ShopKeeperController::class, 'ShopDrivingDelete']);
+        Route::get('/shop/shop_passport/delete/{id}', [ShopKeeperController::class, 'ShopPassportDelete']);
+        Route::get('/shop/shop_cv/delete/{id}', [ShopKeeperController::class, 'ShopCvDelete']);
+        Route::get('/shop/shop_agreement/delete/{id}', [ShopKeeperController::class, 'ShopAgreementDelete']);
+        Route::post('/update-shop/{shop_id}', [ShopKeeperController::class, 'UpdateShop']);
+    });
 });
 
 // ***************************** Employee Dashboard data *******************************
@@ -111,23 +113,22 @@ Route::group(['middleware' => ['role:customer']], function () {
     Route::get('users/profile', [UserController::class, 'UserProfile']);
     Route::get('users/deal-list', [DealController::class, 'UserDealList']);
     Route::get('users/deal', [DealController::class, 'UserDeal']);
-    Route::get('user/logout',[AuthController::class,'UserLogout']);
+    Route::get('user/logout', [AuthController::class, 'UserLogout']);
 });
 // ******************************* ShopKeeper ****************************************
-Route::view('shopkeeper/login','shopkeeper.login')->middleware('guest')->name('shop.login');
-Route::post('shopkeeper/authenticate',[AuthController::class,'ShopKeeperAuth']);
-Route::group(['middleware' => ['role:shopkeeper']], function (){
-    Route::prefix('shopkeeper')->group(function(){
-        Route::get('/customer-search',[ShopKeeperController::class,'SearchCustomer']);
-        Route::get('/shopkeeper-search',[EmployeeController::class,'ShopkeeperSearch']);
-        Route::view('/add-customer','shopkeeper.add-customer');
-        Route::post('/new-customer',[CustomerController::class,'ShopKeeperNewCustomer']);
-        Route::view('/dashboard','shopkeeper.dashboard');
-        Route::get('/logout',[AuthController::class,'ShopLogout']);
-        Route::get('/add-shopkeeper',[ShopKeeperController::class,'CreateNewShopKeeper']);
-        Route::post('/create-shop',[ShopKeeperController::class,'StoreShopKeeper']);
-        Route::get('/customer-report',[CustomerController::class,'ShopkeeperCustomerReport']);
-        Route::get('/shopkeeper-reports',[ShopKeeperController::class,'ShopkeeperCustomerReport']);
-
+Route::view('shopkeeper/login', 'shopkeeper.login')->middleware('guest')->name('shop.login');
+Route::post('shopkeeper/authenticate', [AuthController::class, 'ShopKeeperAuth']);
+Route::group(['middleware' => ['role:shopkeeper']], function () {
+    Route::prefix('shopkeeper')->group(function () {
+        Route::get('/customer-search', [ShopKeeperController::class, 'SearchCustomer']);
+        Route::get('/shopkeeper-search', [EmployeeController::class, 'ShopkeeperSearch']);
+        Route::view('/add-customer', 'shopkeeper.add-customer');
+        Route::post('/new-customer', [CustomerController::class, 'ShopKeeperNewCustomer']);
+        Route::view('/dashboard', 'shopkeeper.dashboard');
+        Route::get('/logout', [AuthController::class, 'ShopLogout']);
+        Route::get('/add-shopkeeper', [ShopKeeperController::class, 'CreateNewShopKeeper']);
+        Route::post('/create-shop', [ShopKeeperController::class, 'StoreShopKeeper']);
+        Route::get('/customer-report', [CustomerController::class, 'ShopkeeperCustomerReport']);
+        Route::get('/shopkeeper-reports', [ShopKeeperController::class, 'ShopkeeperCustomerReport']);
     });
 });

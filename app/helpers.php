742579814c8deef8c2e1f5cf690b9a102083db12
abhishek <webmaster@bestway2india.com>
2023-06-29 18:52:53 +0530

    notification

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Customer;
+use App\Models\Employee;
+use App\Models\GlobalShop;
+use App\Models\Shop;
 use Request as RQ;
 
 if(!function_exists('setActiveClass')){
@@ -7,3 +11,40 @@ if(!function_exists('setActiveClass')){
         return RQ::is($path.'*')?'active':'';
     }
 }
+
+if(!function_exists('InactiveEmployee')){
+    function InactiveEmployee(){
+       return Employee::where('status','0')->count();
+    }
+}
+if(!function_exists('ActiveEmployee')){
+    function ActiveEmployee(){
+       return Employee::where('status','1')->count();
+    }
+}
+
+if(!function_exists('InactiveCustomer')){
+    function InactiveCustomer(){
+       return Customer::where('status','0')->count();
+    }
+}
+
+if(!function_exists('ActiveCustomer')){
+    function ActiveCustomer(){
+       return Customer::where('status','1')->count();
+    }
+}
+
+if(!function_exists('InactiveLocalShop')){
+    function InactiveLocalShop(){
+       return Shop::where('status','0')->count();
+    }
+}
+
+if(!function_exists('InactiveGlobalShop')){
+    function InactiveGlobalShop(){
+       return GlobalShop::where('status','0')->count();
+    }
+}
+
+
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -10,70 +10,73 @@
                         <div class="user-info">
                             <div class="image">
 
-                                <a href="#"><img src="{{asset('admin/light/assets/img/icon/Person.ico')}}" alt="admin"> &nbsp;
+                                <a href="#"><img src="{{ asset('admin/light/assets/img/icon/Person.ico') }}"
+                                        alt="admin"> &nbsp;
                                     <small class="admin">Admin</small></a>
                             </div>
 
                         </div>
                     </li>
-                    <li @if (strpos(Request::url(), "admin/add-employee") !== false)
-                        class="active"
-                    @endif
-                    @if (strpos(Request::url(), "admin/inactive-employee") !== false)
-                    class="active"
-                    @endif
-                    @if (strpos(Request::url(), "admin/active-employee") !== false)
-                        class="active"
-                    @endif><a href="javascript:void(0);" class="menu-toggle"><span>Employee</span></a>
+                    <li @if (strpos(Request::url(), 'admin/add-employee') !== false) class="active" @endif
+                        @if (strpos(Request::url(), 'admin/inactive-employee') !== false) class="active" @endif
+                        @if (strpos(Request::url(), 'admin/active-employee') !== false) class="active" @endif><a href="javascript:void(0);"
+                            class="menu-toggle"><span>Employee</span></a>
                         <ul class="ml-menu">
-                            <li class="{{setActiveClass('admin/add-employee')}}"><a href="{{url('admin/add-employee')}}">Add New</a></li>
-                            <li class="{{setActiveClass('admin/inactive-employee')}}"><a href="{{url('admin/inactive-employee')}}">Inactive Table</a></li>
-                            <li class="{{setActiveClass('admin/active-employee')}}"><a href="{{url('admin/active-employees')}}">Active Table</a></li>
+                            <li class="{{ setActiveClass('admin/add-employee') }}"><a
+                                    href="{{ url('admin/add-employee') }}">Add New</a></li>
+                            <li class="{{ setActiveClass('admin/inactive-employee') }}"><a
+                                    href="{{ url('admin/inactive-employee') }}">Inactive Table @if (InactiveEmployee() > 0)
+                                        <span class="circle">{{ InactiveEmployee() }}</span>
+                                    @endif
+                                </a></li>
+                            <li class="{{ setActiveClass('admin/active-employee') }}"><a
+                                    href="{{ url('admin/active-employees') }}">Active Table @if (ActiveEmployee() > 0)
+                                    <span class="circle">{{ ActiveEmployee() }}</span>
+                                @endif</a></li>
 
                         </ul>
                     </li>
-                    <li @if (strpos(Request::url(), "admin/add-customer") !== false)
-                    class="active"
-                @endif
-                @if (strpos(Request::url(), "inactive-customers") !== false)
-                        class="active"
-                    @endif
-                    @if (strpos(Request::url(), "admin/active-customers") !== false)
-                        class="active"
-                    @endif
-                    ><a href="javascript:void(0);" class="menu-toggle"><span>Customer</span></a>
+                    <li @if (strpos(Request::url(), 'admin/add-customer') !== false) class="active" @endif
+                        @if (strpos(Request::url(), 'inactive-customers') !== false) class="active" @endif
+                        @if (strpos(Request::url(), 'admin/active-customers') !== false) class="active" @endif><a href="javascript:void(0);"
+                            class="menu-toggle"><span>Customer</span></a>
                         <ul class="ml-menu">
-                            <li class="{{setActiveClass('admin/add-customer')}}"><a href="{{url('admin/add-customer')}}">Add New</a></li>
-                            <li class="{{setActiveClass('admin/inactive-customers')}}" ><a href="{{url('admin/inactive-customers')}}">Inactive Table</a></li>
-                            <li class="{{setActiveClass('admin/active-customers')}}"><a href="{{url('admin/active-customers')}}">Active Table</a></li>
+                            <li class="{{ setActiveClass('admin/add-customer') }}"><a
+                                    href="{{ url('admin/add-customer') }}">Add New</a></li>
+                            <li class="{{ setActiveClass('admin/inactive-customers') }}"><a
+                                    href="{{ url('admin/inactive-customers') }}">Inactive Table @if (InactiveCustomer() > 0)
+                                        <span class="circle">{{ InactiveCustomer() }}</span>
+                                    @endif </a></li>
+                            <li class="{{ setActiveClass('admin/active-customers') }}"><a
+                                    href="{{ url('admin/active-customers') }}">Active Table </a></li>
 
                         </ul>
                     </li>
-                    <li
-                    @if (strpos(Request::url(), "admin/local-shop-form") !== false)
-                        class="active"
-                    @endif
-                    @if (strpos(Request::url(), "admin/active-local-shops") !== false)
-                        class="active"
-                    @endif
-                    @if (strpos(Request::url(), "admin/inactive-local-shops") !== false)
-                        class="active"
-                    @endif
-                    @if (strpos(Request::url(), "admin/inactive-global-shops") !== false)
-                        class="active"
-                    @endif
-                    ><a href="javascript:void(0);" class="menu-toggle"><span>Shopkeeper</span></a>
+                    <li @if (strpos(Request::url(), 'admin/local-shop-form') !== false) class="active" @endif
+                        @if (strpos(Request::url(), 'admin/active-local-shops') !== false) class="active" @endif
+                        @if (strpos(Request::url(), 'admin/inactive-local-shops') !== false) class="active" @endif
+                        @if (strpos(Request::url(), 'admin/inactive-global-shops') !== false) class="active" @endif><a href="javascript:void(0);"
+                            class="menu-toggle"><span>Shopkeeper</span></a>
                         <ul class="ml-menu">
                             <li><b>Local</b></li>
-                            <li class="{{setActiveClass('admin/local-shop-form')}}"><a href="{{url('admin/local-shop-form')}}">Add Local Shop</a></li>
-                            <li class="{{setActiveClass('admin/active-local-shops')}}"><a href="{{url('admin/active-local-shops')}}">Active Local Shop</a></li>
-                            <li class="{{setActiveClass('admin/inactive-local-shops')}}"><a href="{{url('admin/inactive-local-shops')}}">Inactive Local Shop</a></li>
+                            <li class="{{ setActiveClass('admin/local-shop-form') }}"><a
+                                    href="{{ url('admin/local-shop-form') }}">Add Local Shop</a></li>
+                            <li class="{{ setActiveClass('admin/active-local-shops') }}"><a
+                                    href="{{ url('admin/active-local-shops') }}">Active Local Shop @if (InactiveLocalShop() > 0)
+                                    <span class="circle">{{ InactiveLocalShop() }}</span>
+                                @endif</a></li>
+                            <li class="{{ setActiveClass('admin/inactive-local-shops') }}"><a
+                                    href="{{ url('admin/inactive-local-shops') }}">Inactive Local Shop</a></li>
 
                             <li><b>Global</b></li>
 
-                            <li class="{{setActiveClass('admin/global-shop-form')}}"><a href="{{url('admin/global-shop-form')}}">Add Global Shop</a></li>
+                            <li class="{{ setActiveClass('admin/global-shop-form') }}"><a
+                                    href="{{ url('admin/global-shop-form') }}">Add Global Shop</a></li>
                             <li><a href="#">Active Global Shop</a></li>
-                            <li class="{{setActiveClass('admin/inactive-global-shops')}}"><a href="{{url('admin/inactive-global-shops')}}">Inactive Global Shop</a></li>
+                            <li class="{{ setActiveClass('admin/inactive-global-shops') }}"><a
+                                    href="{{ url('admin/inactive-global-shops') }}">Inactive Global Shop @if (InactiveGlobalShop() > 0)
+                                    <span class="circle">{{ InactiveGlobalShop() }}</span>
+                                @endif</a></li>
 
                         </ul>
                     </li>
